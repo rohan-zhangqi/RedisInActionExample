b5070d7f16356f806dc5c0c5837afe84a1ef7bda
Qi.Zhang4 <Qi.Zhang4@geely.com>
2020-12-01 08:02:44 +0800

    '增加第二章代码示例'

--- chapter2.py
+++ chapter2.py
@@ -30,10 +30,31 @@ def clean_sessions(conn):
         end_index = min(size - LIMIT, 100)
         tokens = conn.zrange('recent:', 0, end_index - 1)
 
+        # []为python的列表，使用append()方法添加列表项
         session_keys = []
         for token in tokens:
             session_keys.append('viewed:' + token)
 
+        # *号语法可以直接将一连串的多个参数传入函数里面，而不必先对这些参数进行解包（unpack）
         conn.delete(*session_keys)
         conn.hdel('login:', *tokens)
         conn.zrem('recent:', *tokens)
+
+
+# 代码清单2-4 clean_full_sessions()函数
+def clean_full_sessions(conn):
+    while not QUIT:
+        size = conn.zcard('recent:')
+        if size <= LIMIT:
+            time.sleep(1)
+            continue
+        end_index = min(size - LIMIT, 100)
+        sessions = conn.zrange('recent:', 0, end_index - 1)
+
+        session_keys = []
+        for sess in sessions:
+            session_keys.append('viewed' + sess)
+            session_keys.append('cart:' + sess)
+        conn.delete(*session_keys)
+        conn.hdel('login:', *sessions)
+        conn.zrem('recent:', *sessions)
