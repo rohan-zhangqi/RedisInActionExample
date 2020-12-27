7639776ea0c47b0d2820720b7b3d88f8a3809541
Qi.Zhang4 <Qi.Zhang4@geely.com>
2020-12-27 21:14:40 +0800

    增加第七章示例代码

--- chapter7.py
+++ chapter7.py
@@ -7,6 +7,7 @@ import redis
 
 
 # 代码清单7-1 对文档进行标记化处理并创建索引的函数
+# python三引号允许一个字符串跨多行，字符串中可以包含换行符、制表符以及其他特殊字符。
 STOP_WORDS = set('''able about across after all almost also am among
 an and any are as at be because been but by can cannot could dear did
 do does either else ever every for from get got had has have he her
@@ -17,11 +18,20 @@ than that the their them then there these they this tis to too twas us
 wants was we were what when where which while who whom why will with
 would yet you your'''.split())
 
+# 认定单词只能由英文字母和单引号（‘）组成，并且每个单词至少要两个字符长
 WORDS_RE = re.compile("[a-z']{2,}")
 
 
 def tokenize(content):
     words = set()
+    # 在字符串中找到正则表达式所匹配的所有子串，并把它们作为一个迭代器返回。
+    # 语法：
+    # re.finditer(pattern, string, flags=0)
+    # 参数：
+    # pattern：匹配的正则表达式
+    # string：要匹配的字符串。
+    # flags	标志位，用于控制正则表达式的匹配方式，如：是否区分大小写，多行匹配等等。参见：正则表达式修饰符 - 可选标志
+    # 例子：
     for match in WORDS_RE.finditer(content.lower()):
         word = match.group().strip("'")
         if len(word) >= 2:
