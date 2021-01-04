f9ec9beff44420d11f26926804d6841930d12a9d
Qi.Zhang4 <Qi.Zhang4@geely.com>
2021-01-04 20:37:44 +0800

    '增加第七章示例代码'

--- chapter7.py
+++ chapter7.py
@@ -325,6 +325,12 @@ AVERAGE_PER_1K = {}
 TO_ECPM = {
     'cpc': cpc_to_ecpm,
     'cpa': cpa_to_ecpm,
+    # 描述：python 使用 lambda 来创建匿名函数。
+    # lambda只是一个表达式，函数体比def简单很多。
+    # lambda的主体是一个表达式，而不是一个代码块。仅仅能在lambda表达式中封装有限的逻辑进去。
+    # lambda函数拥有自己的命名空间，且不能访问自有参数列表之外或全局命名空间里的参数。
+    # 虽然lambda函数看起来只能写一行，却不等同于C或C++的内联函数，后者的目的是调用小函数时不占用栈内存从而增加运行效率。
+    # 语法：lambda [arg1 [,arg2,.....argn]]:expression
     'cpm': lambda *args : args[-1],
 }
 
@@ -343,6 +349,7 @@ def index_ad(conn, id, locations, content, type, value):
         type = type.encode('latin-1')
 
     for location in locations:
+        # 将广告ID添加到所有相关的位置集合里面
         pipeline.sadd('idx:req:' + location, id)
 
     # return a set()
