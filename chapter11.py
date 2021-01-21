1b9c3d5ace787b377cb1e0cc2bd99a32449dbd75
Qi.Zhang4 <Qi.Zhang4@geely.com>
2021-01-21 17:41:30 +0800

    '增加第十章示例代码'

--- chapter11.py
+++ chapter11.py
@@ -26,3 +26,196 @@ def script_load(script):
         return conn.execute_command(
             "EVAL", script, len(keys), *(keys+args))
     return call
+
+
+# 代码清单11-2 之前在代码清单8-2展示过的创建状态消息散列的函数
+def create_status(conn, uid, message, **data):
+    pipeline = conn.pipeline(True)
+    pipeline.hget('user:%s' % uid, 'login')
+    pipeline.incr('status:id:')
+    login, id = pipeline.execute()
+
+    if not login:
+        return None
+
+    data.update({
+        'message': message,
+        'posted': time.time(),
+        'id': id,
+        'uid': uid,
+        'login': login,
+    })
+    pipeline.hmset('status:%s' % id, data)
+    pipeline.hincrby('user:%s' % uid, 'posts')
+    pipeline.execute()
+    return id
+
+
+_create_status = create_status
+
+
+# 代码清单11-3 使用Lua脚本创建一条状态消息
+def create_status(conn, uid, message, **data):
+    args = [
+        'message', message,
+        'posted', time.time(),
+        'uid', uid,
+    ]
+    for key, value in data.items():
+        args.append(key)
+        args.append(value)
+
+    return create_status_lua(
+        conn, ['user:%s' % uid, 'status:id:'], args)
+
+
+create_status_lua = script_load('''
+local login = redis.call('hget', KEYS[1], 'login')
+if not login then
+    return false
+end
+local id = redis.call('incr', KEYS[2])
+local key = string.format('status:%s', id)
+redis.call('hmset', key,
+    'login', login,
+    'id', id,
+    unpack(ARGV))
+redis.call('hincrby', KEYS[1], 'posts', 1)
+return id
+''')
+
+
+# 代码清单11-4 曾经在6.2.5节中展示过的最终版acquire_lock_with_timeout()函数
+def acquire_lock_with_timeout(
+        conn, lockname, acquire_timeout=10, lock_timeout=10):
+    identifier = str(uuid.uuid4())
+    lockname = 'lock:' + lockname
+    lock_timeout = int(math.ceil(lock_timeout))
+
+    end = time.time() + acquire_timeout
+    while time.time() < end:
+        if conn.setnx(lockname, identifier):
+            conn.expire(lockname, lock_timeout)
+            return identifier
+        elif conn.ttl(lockname) < 0:
+            conn.expire(lockname, lock_timeout)
+
+        time.sleep(.001)
+
+    return False
+
+
+_acquire_lock_with_timeout = acquire_lock_with_timeout
+
+
+# 代码清单11-5 使用Lua重写的acquire_lock_with_timeout()函数
+def acquire_lock_with_timeout(
+        conn, lockname, acquire_timeout=10, lock_timeout=10):
+    identifier = str(uuid.uuid4())                    
+    lockname = 'lock:' + lockname
+    lock_timeout = int(math.ceil(lock_timeout)) 
+
+    acquired = False
+    end = time.time() + acquire_timeout
+    while time.time() < end and not acquired:
+        acquired = acquire_lock_with_timeout_lua(
+            conn, [lockname], [lock_timeout, identifier]) == b'OK'
+
+        time.sleep(.001 * (not acquired))
+
+    return acquired and identifier
+
+
+acquire_lock_with_timeout_lua = script_load('''
+if redis.call('exists', KEYS[1]) == 0 then
+    return redis.call('setex', KEYS[1], unpack(ARGV))
+end
+''')
+
+
+def release_lock(conn, lockname, identifier):
+    pipe = conn.pipeline(True)
+    lockname = 'lock:' + lockname
+
+    while True:
+        try:
+            pipe.watch(lockname)
+            if pipe.get(lockname) == identifier:
+                pipe.multi()
+                pipe.delete(lockname)
+                pipe.execute()
+                return True
+
+            pipe.unwatch()
+            break
+
+        except redis.exceptions.WatchError:
+            pass
+
+    return False
+
+
+_release_lock = release_lock
+
+
+# 代码清单11-6 使用Lua重写的release_lock()函数
+def release_lock(conn, lockname, identifier):
+    lockname = 'lock:' + lockname
+    return release_lock_lua(conn, [lockname], [identifier])
+
+
+release_lock_lua = script_load('''
+if redis.call('get', KEYS[1]) == ARGV[1] then
+    return redis.call('del', KEYS[1]) or true
+end
+''')
+
+
+# 代码清单11-7 来自6.3.1节的acquire_semaphore()函数
+def acquire_semaphore(conn, semname, limit, timeout=10):
+    identifier = str(uuid.uuid4())
+    now = time.time()
+
+    pipeline = conn.pipeline(True)
+    pipeline.zremrangebyscore(semname, '-inf', now - timeout)
+    pipeline.zadd(semname, {identifier: now})
+    pipeline.zrank(semname, identifier)
+    if pipeline.execute()[-1] < limit:
+        return identifier
+
+    conn.zrem(semname, identifier)
+    return None
+
+
+_acquire_semaphore = acquire_semaphore
+
+
+# 代码清单11-8 使用Lua重写的acquire_semaphore()函数
+def acquire_semaphore(conn, semname, limit, timeout=10):
+    now = time.time()
+    return acquire_semaphore_lua(
+        conn, [semname],
+        [now-timeout, limit, now, str(uuid.uuid4())])
+
+
+acquire_semaphore_lua = script_load('''
+redis.call('zremrangebyscore', KEYS[1], '-inf', ARGV[1])
+if redis.call('zcard', KEYS[1]) < tonumber(ARGV[2]) then
+    redis.call('zadd', KEYS[1], ARGV[3], ARGV[4])
+    return ARGV[4]
+end
+''')
+
+
+# 代码清单11-9 使用Lua实现的refresh_semaphore()函数
+def refresh_semaphore(conn, semname, identifier):
+    return refresh_semaphore_lua(
+                conn, [semname], [identifier, time.time()]
+            ) != None
+
+
+refresh_semaphore_lua = script_load('''
+if redis.call('zscore', KEYS[1], ARGV[1]) then
+    return redis.call('zadd', KEYS[1], ARGV[2], ARGV[1]) or tru
+end
+''')
